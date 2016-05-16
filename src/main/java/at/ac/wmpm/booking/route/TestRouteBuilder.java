a07f0d325b86da94bb6cb0a4601253575e42bdc0
Mubashra Akhtar <mubashra.akhtar@hotmail.com>
2016-05-16 20:13:57 +0200

    One route completely implemented

--- src/main/java/at/ac/wmpm/booking/route/TestRouteBuilder.java
+++ src/main/java/at/ac/wmpm/booking/route/TestRouteBuilder.java
@@ -7,13 +7,15 @@ import at.ac.wmpm.booking.service.SaveTestBean;
 // a class used in camel for smart routing within a camel context
 public class TestRouteBuilder extends RouteBuilder{
 
+	//This function builds the route
 	@Override
 	public void configure() throws Exception {
-		// writes something into the database
 		
+		// writes something into the database
 		SaveTestBean savetestbean = new SaveTestBean();
 		
-		
+		from("direct:saveTestBean").process(savetestbean)
+			.to("mongodb:mongoDB?database=booking&collection=tickets&operation=insert");
 	}
 
 }
