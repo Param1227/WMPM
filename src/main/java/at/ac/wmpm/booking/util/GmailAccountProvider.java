547ca79f7ebea5aaec18a877c3a41e6e9961c336
Malinda Prasanjith Perera <e1427345@student.tuwien.ac.at>
2016-06-17 16:54:58 +0200

    Calsses updated

---
+++ src/main/java/at/ac/wmpm/booking/util/GmailAccountProvider.java
@@ -0,0 +1,9 @@
+package at.ac.wmpm.booking.util;
+
+public class GmailAccountProvider {
+	public static final String GMAIL_EMAIL = "wmpm16.g6@gmail.com";
+	public static final String GMAIL_PW = "wmpm2016";
+	public static final String GMAIL_URI = "smtps://smtp.gmail.com?username="+GMAIL_EMAIL+"&password="+GMAIL_PW+"&from="+GMAIL_EMAIL;
+	
+
+}
